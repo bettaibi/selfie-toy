2b1d5a678adc99299ff9c06303a549a9e8c988df
bettaibi <bettaibinidhal00@gmail.com>
2021-02-07 22:46:11 +0100

    project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div style={{padding: "2rem"}}>
+        <button className="btn btn-primary">This Is  Button</button>
+    </div>
+  );
+}
+
+export default App;
